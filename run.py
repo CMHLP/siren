c680d60b83caf6b5f63e1dbf50c03f19311e5390
Aman Phatak <phatakaman@gmail.com>
2023-08-10 13:19:19 +0530

    feat: parse START and END env vars to set date range

--- run.py
+++ run.py
@@ -1,15 +1,22 @@
+from os import getenv
 from sys import argv
 from datetime import datetime, timedelta
 from HindustanTimes.hindustantimes import Scraper
 
 scrapers = {"hindustan_times": Scraper}
-
-print(argv)
 target = scrapers.get(argv[1])
+
+
+def get_dt(env: str, default: datetime):
+    fmt = "%d-%m-%Y"
+    if s := getenv(env):
+        return datetime.strptime(s, fmt)
+    return default
+
+
 if target:
-    weeks = 8 if len(argv) == 2 else int(argv[2])
-    end = datetime.now()
-    start = end - timedelta(weeks=weeks)
+    end = get_dt("END", datetime.now())
+    start = get_dt("START", end - timedelta(weeks=8))
     target(start, end).scrape()
 else:
     raise ValueError("Invalid scraper!")
