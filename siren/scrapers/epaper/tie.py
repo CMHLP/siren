9a0e3a3c3eb179e5d5bddc52b9365d6fdaa4400b
Aman Phatak <phatakaman@gmail.com>
2024-01-10 22:41:16 +0530

    fix: naming and docs

--- siren/scrapers/epaper/tie.py
+++ siren/scrapers/epaper/tie.py
@@ -34,7 +34,7 @@ class TIEMonthSearchProduct(Model):
     id: int
 
 
-class TIEMonthSearchArticle(Model):
+class TIEMonthSearchIssue(Model):
     id: int
     product: TIEMonthSearchProduct
     name: str
@@ -45,10 +45,14 @@ class TIEMonthSearchArticle(Model):
 
 class TIEMonthSearch(Model):
     count: int
-    results: list[TIEMonthSearchArticle] = []
+    results: list[TIEMonthSearchIssue] = []
 
 
 class TIESearchItem(Model):
+    """
+    Represents an item from the /search/issue/{issue_id} endpoint.
+    """
+
     id: str
     pageNum: str
     excerpt: str
@@ -57,6 +61,11 @@ class TIESearchItem(Model):
 
 
 class TIEArticle(Model):
+    """
+    Represents a complete article.
+    The `edition` and `url` fields are not provided by the API, and are injected in the `TIEScraper.scrape` method.
+    """
+
     id: str
     page_num: str
     excerpt: str
@@ -67,15 +76,36 @@ class TIEArticle(Model):
 
 
 class TIESearch(Model):
+    """
+    Represents the search result of the /search/issue/{issue_id} endpoint.
+    """
+
     status: bool
     numFound: int | None = None
     data: list[TIESearchItem] = []
 
 
 class TIEScraper(BaseScraper[TIEArticle]):
+
+    """
+    Scraper for the Times of India E-Paper.
+    """
+
     model = TIEArticle
 
     async def search(self, issue_id: str, keyword: str) -> TIESearch | None:
+        """
+        Search an issue for a keyword. Return a :class:`TIESearch` if the search was successful, else :class:`None`.
+
+        Parameters
+        ----------
+
+        issue_id: :class:`str`
+            The issue ID to search in. (This parameter is a string to match the API.
+
+        keyword: :class:`str`
+            The keyword to search for.
+        """
         url = URL(
             f"https://epaper.indianexpress.com/search/issue/{issue_id}/{keyword}/1"
         )
@@ -88,6 +118,24 @@ class TIEScraper(BaseScraper[TIEArticle]):
     async def get_month(
         self, product_id: int, month: int, year: int
     ) -> TIEMonthSearch | None:
+        """
+        Retrieve a :class:`TIEMonthSearch` containing the articles of the given month.
+        Returns None if unsuccessful.
+
+        Parameters
+        ----------
+
+        product_id: :class:`int`
+            The product ID to search in (each edition has it's own product ID. The `PRODUCTS` dictionary has a mapping of edition name to the product id.)
+
+        month: :class:`int`
+            The month as an integer (1 - 12)
+
+        year: :class:`int`
+            The year as an integer.
+
+        """
+
         url = URL(
             f"https://ecommerce.indianexpress.com/api/partner-delivery-data/?productId={product_id}&month={month}&year={year}&size=31"
         )
@@ -99,6 +147,16 @@ class TIEScraper(BaseScraper[TIEArticle]):
             logger.error(f"Ignoring exception {e} while validating TIEMonthSearch.")
 
     async def scrape_product(self, product_id: int) -> list[TIEArticle]:
+        """
+        Scrape an edition (product).
+        Return a list of :class:`TIEArticle`.
+
+        Parameters
+        ----------
+
+        product_id: :class:`int`
+            The ID of the Product to scrape.
+        """
         cur = self.start
         month_tasks: list[asyncio.Task[TIEMonthSearch | None]] = []
         while cur <= self.end:
@@ -110,12 +168,12 @@ class TIEScraper(BaseScraper[TIEArticle]):
                 cur = cur.replace(month=m)
             else:
                 cur = cur.replace(month=1, year=cur.year + 1)
-        articles: dict[str, TIEMonthSearchArticle] = {}
+        issues: dict[str, TIEMonthSearchIssue] = {}
         search_tasks: list[asyncio.Task[TIESearch | None]] = []
         for task in asyncio.as_completed(month_tasks):
             if search := await task:
                 for article in search.results:
-                    articles[article.issue_id] = article
+                    issues[article.issue_id] = article
                     for keyword in self.keywords:
                         search_tasks.append(
                             asyncio.create_task(self.search(article.issue_id, keyword))
@@ -125,7 +183,7 @@ class TIEScraper(BaseScraper[TIEArticle]):
         for task in asyncio.as_completed(search_tasks):
             if search := await task:
                 for item in search.data:
-                    month_article = articles[item.issue_id]
+                    issue = issues[item.issue_id]
                     results.append(
                         TIEArticle(
                             id=item.issue_id,
@@ -133,8 +191,8 @@ class TIEScraper(BaseScraper[TIEArticle]):
                             excerpt=item.excerpt.replace("<strong>", "").replace(
                                 "</strong>", ""
                             ),
-                            name=month_article.name,
-                            publish_date=month_article.publish_date
+                            name=issue.name,
+                            publish_date=issue.publish_date
                             + timedelta(
                                 days=1
                             ),  # for some reason the publish date is a day behind
